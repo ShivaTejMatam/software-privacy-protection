fe611464a8246a23ade91fb71e798afeea44df5a
Mahanth <71886381+Mahanth-Maha@users.noreply.github.com>
2021-05-27 12:48:39 +0530

    small change

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import hashlib
 
-class RSA:
-    def PasswordHasher(self, file):
-        return hashlib.sha256(file.encode()).hexdigest()
-
 if __name__=='__main__':
   print("Select Your File")
   File_path = input("Enter path of file")
   File = open(File_path , "r")
   try:
+    hash = hashlib.sha256(file.encode()).hexdigest()
     i = int(input("1 . Generate Hash \n2 . Check Hash"))
     if i == 1:
-      hasher = RSA()
-      hash = hasher.PasswordHasher(File)
       print("Your hash :",hash)
     elif i == 2:
       j = ("Paste the hash")
-      hash = hasher.PasswordHasher(File)
       if j == hash:
         print("File not corrupted")
       else:
